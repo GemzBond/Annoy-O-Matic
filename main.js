aa653f9624ef3cc81da155c02fc6c7489d24d4fe
Regem Semilla <regem.semilla123@gmail.com>
2019-03-05 17:40:22 +0800

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var answer = prompt("Are we there yet?");
-
-while (answer.indexOf("yes") === -1 && answer.indexOf("yes") === -1) {
-    var answer = prompt("Are we there yet?");
-}
-
-alert("Yey! We made it!");
